749c94f35d5be87fbb45453bd739d87dcce979ca
oh-facts <as17793@nyu.edu>
2023-12-16 16:25:55 -0500

    win32 shenanigans

--- src/main.c
+++ src/main.c
@@ -1,8 +1,60 @@
-#include <yk.h>
+#include <windows.h>
 
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
-    printf("hello :)");
+    HINSTANCE hinstance;
+    HWND hwnd;
+
+    hinstance = GetModuleHandle(NULL);
+
+    WNDCLASS wc = {0};
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hinstance;
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = "MainWindowClass";
+
+
+    if (!RegisterClass(&wc))
+        return FALSE;
+
+
+    hwnd = CreateWindowA("MainWindowClass", "Window Name", WS_OVERLAPPEDWINDOW,
+                         CW_USEDEFAULT, CW_USEDEFAULT, 600, 600,
+                         NULL, NULL, hinstance, NULL);
+
+    if (!hwnd)
+        return FALSE;
+
+
+    ShowWindow(hwnd, SW_SHOWNORMAL);
+    UpdateWindow(hwnd);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+
     return 0;
 }
--- src/yk.h
+++ src/yk.h
@@ -3,5 +3,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <Windows.h>
+
+typedef struct YK_window YK_window;
+
+struct YK_window
+{
+
+};
 
 #endif
