fba2ae0a753228444c7f0f6e4f5c0ccb4789e262
oh-facts <as17793@nyu.edu>
2024-01-01 18:49:57 -0500

    stop works

--- src/win32_window.cpp
+++ src/win32_window.cpp
@@ -114,3 +114,29 @@ void yk_free_window(YkWindow* window)
     DestroyWindow(window->win_handle);
 }
 
+/*
+struct _YkTime
+{
+    LARGE_INTEGER freq;
+    LARGE_INTEGER start;
+};
+
+void yk_time_innit(YkTime* time)
+{
+    _YkTime* _time = (_YkTime*)time;
+    LARGE_INTEGER now = {};
+    QueryPerformanceCounter(&now);
+
+    _time->start = now;
+    QueryPerformanceFrequency(&_time->freq);
+}
+
+f32 yk_get_time(YkTime* time)
+{
+    _YkTime* _time = (_YkTime*)time;
+    LARGE_INTEGER now = {};
+    QueryPerformanceCounter(&now);
+
+    return ((f32)now.QuadPart - _time->start.QuadPart) / _time->freq.QuadPart;
+}
+*/
